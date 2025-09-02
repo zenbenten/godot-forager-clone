edf1c0ec2b5d1d158bacf0466848b83889762415
zenbenten <111924967+zenbenten@users.noreply.github.com>
2025-09-02 11:07:53 -0400

    Update item_data.gd

--- scripts/data/item_data.gd
+++ scripts/data/item_data.gd
@@ -1,1 +1,8 @@
-extends Node
+class_name ItemData
+extends Resource
+
+@export var id: String
+@export var display_name: String
+@export var description: String
+@export var icon: Texture2D
+@export var max_stack_size: int = 64
