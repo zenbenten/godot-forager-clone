692ba4498f2238cb6bc699f49d6dd6e61200f20e
zenbenten <joshuaelsner97@gmail.com>
2025-09-26 12:53:55 -0400

    feat(build): updated build_manager.gd

--- scripts/data/building_data.gd
+++ scripts/data/building_data.gd
@@ -2,3 +2,8 @@ class_name BuildingData
 extends Resource
 
 @export var building_name: String
+# The scene to instance when this is built
+@export var building_scene: PackedScene
+#A dictionary of required items and quantities
+#key= itemdata resource value= int quantity
+@export var ingredients: Dictionary
